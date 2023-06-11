2824d52d4d7614c0b69c2fc1267f50aeb4759286
Blurry Face <kakashi.code.take@gmail.com>
2023-06-11 22:38:12 +0530

    Started imtegrating Thunk. Yet to dispatch

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import MovieListing from '../MovieListing/MovieListing'
-import movieApi from '../../common/apis/movieApi'
-import {MovieApiKey} from '../../common/apis/MovieApiKey'
+
 import { useDispatch } from 'react-redux';
 import { addMovies } from '../../features/movies/movieSlice';
 
@@ -11,12 +10,7 @@ const Home = () => {
 
     useEffect(()=>{
         const fetchMovies = async() =>{             
-            const response = await movieApi
-            .get(`?apiKey=${MovieApiKey}&s=${movieText}&type=movie`)
-            .catch((err)=>{
-                console.log(err)
-            })
-            dispatch(addMovies(response.data))
+            
         }
         fetchMovies();
     },[])
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,5 +1,15 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import movieApi from '../../common/apis/movieApi'
+import {MovieApiKey} from '../../common/apis/MovieApiKey'
 
+const fetchAsyncMovies = createAsyncThunk('movies/fetchAsyncMovies', async ()=>{
+    const response = await movieApi
+            .get(`?apiKey=${MovieApiKey}&s=${movieText}&type=movie`)
+            .catch((err)=>{
+                console.log(err)
+            })
+            dispatch(addMovies(response.data))
+})
 const initialState ={
     movies:{},
 }
