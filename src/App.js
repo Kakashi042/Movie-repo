15509d02a5836fc9a965f6e3f273bfa675386cc2
Blurry Face <kakashi.code.take@gmail.com>
2023-06-11 17:05:41 +0530

    Completed till movieCard. Yet to update the card details

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.scss';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
-import MovieListing from './components/MovieListing/MovieListing';
+import MovieDetail from './components/MovieDetail/MovieDetail';
 import PageNotFound from './components/PageNotFound/PageNotFound';
 
 function App() {
@@ -12,11 +12,13 @@ function App() {
     <div className="App">
       <Router>
         <Header />
+        <div className='container'>
         <Routes>
           <Route path='/' element={<Home/>}/>
-          <Route path='/movie/:imdbID' element={<MovieListing />} />
+          <Route path='/movie/:ID' element={<MovieDetail />} />
           <Route path='*' element={<PageNotFound/>} /> 
         </Routes>
+        </div>
         <Footer />
       </Router>
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import user from '../../images/user.png'; 
+import { Link } from 'react-router-dom';
+import './Header.scss'
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className='header'>
+            <Link to='/'>
+            <div className='logo'>MovieKart</div>
+            </Link>
+            <div className='user-image'>
+                <img src={user} alt='user image'/>
+            </div>
         </div>
     );
 };
