6f2ade357caec27283944b1cfc4406543016fb29
Blurry Face <kakashi.code.take@gmail.com>
2023-06-16 10:29:14 +0530

    MovieDetails are updated. Sass in progress

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -4,19 +4,62 @@ import { useParams } from 'react-router-dom';
 import { fetchAsyncMovieOrShowDetail, getSelectedMovieOrShow } from '../../features/movies/movieSlice';
 
 const MovieDetail = () => {
-    const { id } = useParams();
-    console.log(id);
+    const {id}  = useParams();
 
     const dispatch = useDispatch();
     const data = useSelector(getSelectedMovieOrShow)
-    console.log(data);
+    
     useEffect(()=>{
         dispatch(fetchAsyncMovieOrShowDetail(id));
     },[dispatch, id])
 
     return (
-        <div>
-            MovieDetail
+        <div className='movie-section'>
+            <div className='section-left'>
+                <div className='movie-title'>{data.Title}</div>
+                <div className='movie=rating'>
+                    <span>
+                        IMDB Rating <i className='fa fa-star'></i> : {data.imdbRating}
+                    </span>
+                    <span>
+                        IMDB Votes <i className='fa fa-thumbs-up'></i> : {data.imdbVotes}
+                    </span>
+                    <span>
+                        Runtime <i className='fa fa-film'></i> : {data.Runtime}
+                    </span>
+                    <span>
+                        Year <i className='fa fa-calendar'></i> : {data.Year}
+                    </span>
+                </div>
+                <div className='movie-plot'>
+                    {data.Plot}
+                </div>
+                <div className='movie-info'>
+                    <div>
+                        <span>Director</span>
+                        <span> {data.Director}</span>
+                    </div>
+                    <div>
+                        <span>Cast</span>
+                        <span> {data.Actors}</span>
+                    </div>
+                    <div>
+                        <span>Genre</span>
+                        <span>{data.Genre}</span>
+                    </div>
+                    <div>
+                        <span>Languages</span>
+                        <span>{data.Language}</span>
+                    </div>
+                    <div>
+                        <span>Awards</span>
+                        <span>{data.Awards}</span>
+                    </div>
+                </div>
+            </div>
+            <div className='section-right'>
+                <img src={data.Poster} alt={data.Title} />
+            </div>
         </div>
     );
 };
